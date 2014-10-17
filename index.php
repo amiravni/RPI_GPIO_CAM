eed864f3f22a649815494df1d804e07f9d9c2d03
amiravni <amiravni83@gmail.com>
2014-10-17 16:44:29 +0300

    Attachment Works

--- index.php
+++ index.php
@@ -1,66 +1,26 @@
 <?php 
+require_once('./PHPMailer/class.phpmailer.php');
 echo "Start... <br/>";
 $myfile = fopen("TurnOn.txt", "w");
 fclose($myfile);
 echo "Waiting 5 seconds... <br/>";
 sleep(5);
 
-//define the receiver of the email 
-$to = 'amiravni83@gmail.com'; 
-//define the subject of the email 
-$subject = 'Test email with attachment'; 
-//create a boundary string. It must be unique 
-//so we use the MD5 algorithm to generate a random hash 
-$random_hash = md5(date('r', time())); 
-//define the headers we want passed. Note that they are separated with \r\n 
-$headers = "From: webmaster@example.com\r\nReply-To: webmaster@example.com"; 
-//add boundary string and mime type specification 
-$headers .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_hash."\""; 
-//read the atachment file contents into a string,
-//encode it with MIME base64,
-//and split it into smaller chunks
-$attachment = chunk_split(base64_encode(file_get_contents('output0.avi'))); 
-//define the body of the message. 
-ob_start(); //Turn on output buffering 
-?> 
---PHP-mixed-<?php echo $random_hash; ?>  
-Content-Type: multipart/alternative; boundary="PHP-alt-<?php echo $random_hash; ?>" 
+$bodytext = "Attached Output from Cam";
+$email = new PHPMailer();
+$email->From      = 'AmirPI@myPI.com';
+$email->FromName  = 'AmirFromPI';
+$email->Subject   = 'Video From Cam';
+$email->Body      = $bodytext;
+$email->AddAddress('amiravni83@gmail.com');
 
---PHP-alt-<?php echo $random_hash; ?>  
-Content-Type: text/plain; charset="iso-8859-1" 
-Content-Transfer-Encoding: 7bit
+$file_to_attach = './output0.avi';
 
-Hello World!!! 
-This is simple text email message. 
+$email->AddAttachment( $file_to_attach , 'Caught.avi' );
 
---PHP-alt-<?php echo $random_hash; ?>  
-Content-Type: text/html; charset="iso-8859-1" 
-Content-Transfer-Encoding: 7bit
-
-<h2>Hello World!</h2> 
-<p>This is something with <b>HTML</b> formatting.</p> 
-
---PHP-alt-<?php echo $random_hash; ?>-- 
-
---PHP-mixed-<?php echo $random_hash; ?>  
-Content-Type: application/zip; name="attachment.zip"  
-Content-Transfer-Encoding: base64  
-Content-Disposition: attachment  
-
-<?php echo $attachment; ?> 
---PHP-mixed-<?php echo $random_hash; ?>-- 
-
-<?php 
-//copy current buffer contents into $message variable and delete current output buffer 
-$message = ob_get_clean(); 
-//send the email 
-$mail_sent = @mail( $to, $subject, $message, $headers ); 
-//if the message is sent successfully print "Mail sent". Otherwise print "Mail failed" 
-echo $mail_sent ? "Mail sent" : "Mail failed"; 
-
-//mail('amiravni83@gmail.com', 'Try', 'trytrytry');
-//system("sudo /usr/bin/python /var/www/myInputSketch.py");
+ echo $email->Send() ? "Mail sent" : "Mail failed"; 
 echo "end...";
+
 //$command1 = escapeshellcmd("sudo /usr/bin/python /var/www/myInputSketch.py");
 //$output = shell_exec($command1);
 //echo $output;
